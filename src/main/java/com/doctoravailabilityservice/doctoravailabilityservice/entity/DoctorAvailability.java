5cf96fcbd2725d47a3c7f8a89e7712648327a6e3
Nikila Fernando <nikilafernando@Nikilas-MacBook-Pro.local>
2023-07-02 01:25:40 +0530

    Adding nullable = false constraint to  always provided when saving data to the database.

--- src/main/java/com/doctoravailabilityservice/doctoravailabilityservice/entity/DoctorAvailability.java
+++ src/main/java/com/doctoravailabilityservice/doctoravailabilityservice/entity/DoctorAvailability.java
@@ -2,6 +2,9 @@ package com.doctoravailabilityservice.doctoravailabilityservice.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Entity
 @Table(name = "doctor")
 
@@ -11,34 +14,86 @@ public class DoctorAvailability {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "doctor_id")
+    @Column(name = "doctor_id", nullable = false)
     private Integer doctorId;
 
-    @Column(name = "docDate")
-    private String docDate;
+    @Column(name = "availability_date", nullable = false)
+    private LocalDate availabilityDate;
 
-    @Column(name = "start_time")
-    private String startTime;
+    @Column(name = "start_time", nullable = false)
+    private LocalTime startTime;
 
-    @Column(name = "end_time")
-    private String endTime;
+    @Column(name = "end_time", nullable = false)
+    private LocalTime endTime;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status")
+    @Column(name = "status", nullable = false)
     private Status status;
 
     @Column(name = "reason")
     private String reason;
 
-    public void setDoctorId(Integer doctorId) {
-    }
-
-    public void setId(Integer availabilityId) {
-    }
-
     public enum Status {
         AVAILABLE,
         UNAVAILABLE
+    }
+
+    // getters
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getDoctorId() {
+        return doctorId;
+    }
+
+    public LocalDate getAvailabilityDate() {
+        return availabilityDate;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    // setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setDoctorId(Integer doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public void setAvailabilityDate(LocalDate availabilityDate) {
+        this.availabilityDate = availabilityDate;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
     }
 
 
